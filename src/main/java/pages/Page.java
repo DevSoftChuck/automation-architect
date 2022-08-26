ea3a0089c9f9e5a42da9a6337a8e7e218140e60a
Ivan Andraschko <ivan.andraschko@gmail.com>
2022-08-25 21:18:16 -0400

    Enhancement

--- src/main/java/pages/Page.java
+++ src/main/java/pages/Page.java
@@ -6,13 +6,15 @@ public abstract class Page{
 
     private TestBuilder testBuilder;
 
-    //Inits all the pages, use this method if you prefer
     protected void init(TestBuilder testBuilder){
         this.testBuilder = testBuilder;
     }
 
-    /*  This method allows us to recover control of our TestBuilder instance, in case we have used the newInstance
-       method above and created another object to call a method that does not return the TestBuilder instance. */
+    /**
+     *  This method allows us to recover control of our TestBuilder instance,
+     *  in case we have used the newInstance method above and created another
+     *  object to call a method that does not return the TestBuilder instance.
+     */
     public TestBuilder switchBackToTestBuilder(){
         return this.testBuilder;
     }
